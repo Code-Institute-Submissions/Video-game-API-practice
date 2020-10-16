39aeb95ee4e5ebee19dc718c183a1f13a27ac645
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2020-10-16 13:46:29 +0000

    changed xhr request to ajax request

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,35 +8,18 @@ $(document).ready(() => {
 
 function getGames(searchText) {
 
-    const data = null;
-
-    const xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === this.DONE) {
-            console.log(JSON.parse(xhr.response));
-            let games = JSON.parse(xhr.response);
-            let output = '';
-            $.each(games, (id, game) => {
-                output += `
-                <div class="col-md-3>"
-                    <div class="well text-center">
-                        <img src="${game.background_image}">
-                        <h5>${game.name}</h5>
-                        <a onclick="gameSelected('${game.metacritic_url})" class="btn btn-primary" href="#">Game Details</a>
-                    </div>
-                </div>
-                `;
-            })
-            
-            $('games').html(output);
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://rapidapi.p.rapidapi.com/games/" + searchText,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+            "x-rapidapi-key": "e820b60717mshf9de36d3c2a66b8p16a209jsnbbb441546d84"
         }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
     });
-
-    xhr.open("GET", "https://rapidapi.p.rapidapi.com/games?" + searchText);
-    xhr.setRequestHeader("x-rapidapi-host", "rawg-video-games-database.p.rapidapi.com");
-    xhr.setRequestHeader("x-rapidapi-key", "e820b60717mshf9de36d3c2a66b8p16a209jsnbbb441546d84");
-
-    xhr.send(data);
 }
