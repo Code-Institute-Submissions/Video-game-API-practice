8917ad4acece38e7e17bd09f72a50323e60efdf8
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2020-10-18 10:29:36 +0000

    changed background colors and adjusted margins, padding and image sizes for index html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// Runs the getGames function when you click the submit button on the search bar taking in searchText as a parameter
+
 $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
         let searchText = $('#searchText').val();
@@ -6,12 +8,15 @@ $(document).ready(() => {
     });
 });
 
+// Retrieves a list of games from the API based on the search text entered and submitted in the search Bar 
+// and post them on the index page. Searches for strings.
+
 function getGames(searchText) {
 
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://rapidapi.p.rapidapi.com/games/" + searchText,
+        "url": "https://rapidapi.p.rapidapi.com/games?search=" + searchText,
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
@@ -21,5 +26,26 @@ function getGames(searchText) {
 
     $.ajax(settings).done(function (response) {
         console.log(response);
+        let games = response;
+        let gamesResults = games.results;
+        
+        let output = '';
+        $.each(gamesResults, (key, game) => {
+            console.log(key, game);
+            output += `
+            <div class="col-md-4 no-padding">
+                <div class="text-center">
+                    <img class="thumbnails" src="${game.background_image}">
+                    <h5>${game.name}</h5>
+                    <p>Metacritic score - ${game.metacritic}</p>
+                </div>
+            </div>`
+        });
+        document.getElementById("gamediv").innerHTML = output;
+    })
+
+    .catch((err)  => {
+        console.log(err);
     });
 }
+
