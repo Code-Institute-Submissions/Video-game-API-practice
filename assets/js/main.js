73188de0b3e2fd25f6005e224935ed532cc177ae
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2020-10-20 21:48:36 +0000

    added a for each loop that populates an array with developer names and then writes it to the document

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ function getGames(searchText) {
         console.log(response);
         let games = response;
         let gamesResults = games.results;
-        
+
         let output = '';
         $.each(gamesResults, (key, game) => {
             console.log(key, game);
@@ -45,45 +45,71 @@ function getGames(searchText) {
         });
     })
 
-    .catch((err)  => {
-        console.log(err);
-    });
+        .catch((err) => {
+            console.log(err);
+        });
 }
 
 // saves the session id to carry over to the game.html page opens the page when you click the game details button
 
-function gameSelected(id){
+function gameSelected(id) {
     sessionStorage.setItem('gameID', id);
     window.location = 'game.html';
     return false;
 }
 
+// transfers the object of the selected game to the game.html page and displays the data onto the page
 
-function getGame(){
+function getGame() {
     let gameID = sessionStorage.getItem('gameID');
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://rapidapi.p.rapidapi.com/games/" + gameID,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+            "x-rapidapi-key": "e820b60717mshf9de36d3c2a66b8p16a209jsnbbb441546d84"
+        }
+    };
+
     $.ajax(settings).done(function (response) {
         console.log(response);
-        let games = response;
-        let gamesResults = games.results;
-        
-        let output = '';
-        $.each(gamesResults, (key, game) => {
-            console.log(key, game);
-            output += `
+
+        let selectedGame = response;
+        let gameDevs = selectedGame.developers;
+        console.log(gameDevs);
+        let devNameArray = [];
+        gameDevs.forEach(function (devs) {
+           
+            let devNames = devs.name;
+            console.log(devNames);
+            devNameArray.push(devNames);
+            console.log(`ARRAY: ${devNameArray}`);
+        });
+
+        let output = `
             <div class="col-lg-4 col-md-6 no-padding">
                 <div class="text-center">
-                    <img class="thumbnails" src="${game.background_image}">
-                    <h5>${game.name}</h5>
-                    <p>Metacritic score - ${game.metacritic}</p>
-                    <a onclick="gameSelected('${game.id}')" class="btn btn-success href="#">Game Details</a>
+                    <img class="thumbnails" src="${selectedGame.background_image}">
+                    <h5>${selectedGame.name}</h5>
+                    <div>Description - ${selectedGame.description}</div>
+                    <div>Developers - <p>${devNameArray}</p></div>
+                    <p>Released - ${selectedGame.released}</p>
+                    <p>Metacritic score - ${selectedGame.metacritic}</p>
+                    <p>ratings - ${selectedGame.ratings}</p>
+                    <a href="${selectedGame.website}" target=_blank>Official Website</a><br>
+                    <a onclick="gameSelected('${selectedGame.website}')" class="btn btn-success href="#">Screenshots</a>
                 </div>
             </div>`
-            document.getElementById("gamediv").innerHTML = output;
-        });
-    })
+        document.getElementById("selected-div").innerHTML = output;
 
-    .catch((err)  => {
-        console.log(err);
-    });
-    
+
+
+
+    })
+        .catch((err) => {
+            console.log(err);
+        });
 }
+
