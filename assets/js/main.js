8dd787e0eb8fc55a71b9935b597d0f0950aa1f5b
joshhunt1991 <Joshhunt_1991@hotmail.co.uk>
2020-10-31 11:08:33 +0000

    added function that searches for a randomly generated game ID

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,3 +178,47 @@ function getGame() {
 }
 
 
+
+function randomGames() {
+
+let randomID = Math.floor(Math.random() * 462141) + 1;
+console.log(randomID);
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://rapidapi.p.rapidapi.com/games/" + randomID,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+            "x-rapidapi-key": "e820b60717mshf9de36d3c2a66b8p16a209jsnbbb441546d84"
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        let games = response;
+        let gamesResults = games.results;
+
+        let output = '';
+        $.each(gamesResults, (key, game) => {
+            console.log(key, game);
+            output += `
+            <div class="col-lg-4 col-md-6 no-padding">
+                <div class="text-center">
+                    <img class="thumbnails" src="${game.background_image}">
+                    <h5>${game.name}</h5>
+                    <p>Metacritic score - ${game.metacritic}</p>
+                    <a onclick="gameSelected('${game.id}')" class="btn btn-success detail-btn href="#">Game Details</a>
+                </div>
+            </div>`
+            document.getElementById("randomGamediv").innerHTML = output;
+        });
+    })
+
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+
+
